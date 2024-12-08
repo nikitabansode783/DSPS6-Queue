07630101136d3dc36d0076c997518b58bac33744
nikitabansode783 <nikitabansode783@gmail.com>
2024-12-08 15:07:54 +0530

    queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,131 +1,103 @@
-/*Nikita Bansode ,Roll no.:10,DSPS Assgnminet no.:6,26-9-24*/
-#include<iostream>
-
+/* Nikita Bansode, Roll no.:10, DSPS Assignment no.:6, 26-9-24 */
+#include <iostream>
 using namespace std;
-int n,i;
-class queue{
-   int F,R;
-   int q[10];
-  
-   public:
-   queue(){
-    F=-1;
-    R=-1;
-   }
-   void enqueue();
-   void dequeue();
-   void check();
-   void display();
-void peak();
 
+class Queue {
+    int front, rear, size;
+    int q[10];
 
+public:
+    Queue() : front(-1), rear(-1), size(10) {}
+
+    void enqueue() {
+        int ele;
+        cout << "Enter the element: ";
+        cin >> ele;
+
+        if (rear == size - 1) {
+            cout << "Queue is full" << endl;
+        } else {
+            if (front == -1) front = 0;
+            q[++rear] = ele;
+            cout << "Enqueued: " << ele << endl;
+        }
+    }
+
+    void dequeue() {
+        if (front == -1) {
+            cout << "Queue is empty" << endl;
+        } else {
+            cout << "Dequeued: " << q[front] << endl;
+            if (front == rear) {
+                front = rear = -1;
+            } else {
+                front++;
+            }
+        }
+    }
+
+    void check() {
+        if (front == -1) {
+            cout << "Queue is empty" << endl;
+        } else if (rear == size - 1) {
+            cout << "Queue is full" << endl;
+        } else {
+            cout << "Queue has elements" << endl;
+        }
+    }
+
+    void display() {
+        if (front == -1) {
+            cout << "Queue is empty" << endl;
+        } else {
+            cout << "Queue elements: ";
+            for (int i = front; i <= rear; i++) {
+                cout << q[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void peak() {
+        if (front == -1) {
+            cout << "Queue is empty" << endl;
+        } else {
+            cout << "Front element: " << q[front] << endl;
+        }
+    }
 };
 
-void queue::enqueue(){
-int ele;
-    cout<<"enter the element "<<endl;
-    cin>>ele;
-    if(R==-1){
-        R=0;
-        F=0;
-        q[R]=ele;
-    }
-    else if(R==n-1)  {
-        cout<<"Queue is full";
+int main() {
+    Queue q;
+    int ch;
 
-    }  
-    else{
-        R++;
-        q[R]=ele;
+    do {
+        cout << "\nMenu:\n1. Enqueue\n2. Dequeue\n3. Check\n4. Peak\n5. Display\n6. Exit\nEnter choice: ";
+        cin >> ch;
 
-    }
-}
-void queue::dequeue(){
-if(R==-1 && F==-1){
-    cout<<"\nQueue is Empty";
+        switch (ch) {
+            case 1:
+                q.enqueue();
+                break;
+            case 2:
+                q.dequeue();
+                break;
+            case 3:
+                q.check();
+                break;
+            case 4:
+                q.peak();
+                break;
+            case 5:
+                q.display();
+                break;
+            case 6:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice!" << endl;
+        }
+    } while (ch != 6);
 
-}
-else if(F==R){
-cout<<"\ndelete element"<<q[F];
-F=-1;
-R=-1;
-}
-else{
-cout<<"\ndeleted element is "<<q[F];
-F++;
-
-}
-}
-void queue::check(){
-   if (R == -1&&F==-1) {
-        cout << "\nQueue is empty." << endl;
-    } else if (R == n - 1) {
-        cout << "\nQueue is full." << endl;
-    } else {
-        cout << "\nQueue has elements." << endl;
-    }
-
-}
-
-
-
-void queue::display(){
-    if(R==-1 && F==-1){
-        cout<<"\nqueu is empty";
-    }
-    else if(F==R){
-    cout<<"\nelements are";
-    cout<<q[i];
-    }
-    else{
-    for(i=F;i<R;i++){
-    
-    cout<<q[i];
-}
-}
-}
-void queue::peak(){
-if(R==-1){
-    cout<<"\nQueue is empty";
-
-}
-else{
-    cout<<"\nfront element"<<q[F];
-}
-}
-int main(){
-    queue q;
-    int n,ch;
-    cout<<"\nEnter the size elements";
-    cin>>n;
-    do{
-    cout<<"\nmenu";
-    cout<<"\n 1.Enqueue \n2.Dequeue \n3.check \n4.peak \n5.display"<<endl;
-    cout<<"\nenter choice"<<endl;
-    cin>>ch;
-    switch(ch){
-    case 1:
-    
-    q.enqueue();
-    
-     
-    break;
-    case 2:
-      q.dequeue();
-     
-    break;
-    case 3:
-    q.check();
-    break;
-     case 4:
-    q.peak();
-    break;
-    
-    case 5:
-    
-    q.display();
-    break;
-    }
-    }while(ch!=7);
-return 0;
+    return 0;
 }
